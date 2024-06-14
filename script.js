f6f1c87df8bc34244cb9b7f17d160fa7baa4d75a
Bruno.Furtado <brunofurtado@alunos.utfpr.edu.br>
2024-06-14 20:25:58 -0300

    mod03

--- script.js
+++ script.js
@@ -1,53 +1,26 @@
-document.getElementById('pertenceIgreja').addEventListener('change', function() {
-    var igrejaDiv = document.getElementById('igrejaDiv');
-    if (this.value === 'sim') {
-        igrejaDiv.style.display = 'block';
-    } else {
-        igrejaDiv.style.display = 'none';
-    }
-});
+const url = 'https://script.google.com/macros/s/AKfycbygV8HR08cZiVolxt0En6DE7CG9clK_BS7mKgrHRkXfdJ9aRrBa-88bfCkAF2rqmXSW/exec';
+const dados = document.forms['louvorzao'];
 
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var nome = document.getElementById('nome').value;
-    var dataNascimento = document.getElementById('dataNascimento').value;
-    var telefone = document.getElementById('telefone').value;
-    var pertenceIgreja = document.getElementById('pertenceIgreja').value;
-    var igreja = pertenceIgreja === 'sim' ? document.getElementById('igreja').value : 'Não pertence a nenhuma igreja';
-    
-    var id = 'ID' + Math.floor(Math.random() * 1000000); // Gerar um ID aleatório
-    
-    var data = {
-        id: id,
-        nome: nome,
-        dataNascimento: dataNascimento,
-        telefone: telefone,
-        pertenceIgreja: pertenceIgreja,
-        igreja: igreja
-    };
-    
-    // URL do script do Google Apps
-    var scriptURL = 'https://script.google.com/macros/s/AKfycbygV8HR08cZiVolxt0En6DE7CG9clK_BS7mKgrHRkXfdJ9aRrBa-88bfCkAF2rqmXSW/exec';
+dados.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-    fetch(scriptURL, {
+    fetch(url, {
         method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
+        body: new FormData(dados)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao enviar os dados. Status do erro: ' + response.status);
         }
-    }).then(response => response.json())
-      .then(data => {
-          if (data.status === 'success') {
-              var mensagem = `Nome: ${nome}<br>Data de Nascimento: ${dataNascimento}<br>Número de Telefone: ${telefone}<br>Pertence a igreja: ${pertenceIgreja}<br>${pertenceIgreja === 'sim' ? 'Igreja: ' + igreja : ''}<br><br>ID de Registro: ${id}`;
-              document.getElementById('mensagem').innerHTML = mensagem;
-              document.getElementById('resultado').style.display = 'block';
-              document.getElementById('registrationForm').reset();
-          } else {
-              alert('Ocorreu um erro ao salvar os dados. Por favor, tente novamente.');
-          }
-      }).catch(error => {
-          console.error('Erro:', error);
-          alert('Ocorreu um erro ao salvar os dados. Por favor, tente novamente.');
-      });
+        return response.text(); // Retorna o texto da resposta
+    })
+    .then(data => {
+        console.log('Dados enviados com sucesso!', data);
+        dados.reset();
+        // Aqui você pode mostrar uma mensagem de sucesso para o usuário, se desejar
+    })
+    .catch(error => {
+        console.error('Erro no envio dos dados!', error);
+        // Aqui você pode mostrar uma mensagem de erro para o usuário, se desejar
+    });
 });
