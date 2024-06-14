1b6dce6a7efbf324e289335e25d7d49611f6cde0
Bruno.Furtado <brunofurtado@alunos.utfpr.edu.br>
2024-06-14 19:25:34 -0300

    mod01

--- script.js
+++ script.js
@@ -18,9 +18,36 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     
     var id = 'ID' + Math.floor(Math.random() * 1000000); // Gerar um ID aleatório
     
-    var mensagem = `Nome: ${nome}<br>Data de Nascimento: ${dataNascimento}<br>Número de Telefone: ${telefone}<br>Pertence a igreja: ${pertenceIgreja}<br>${pertenceIgreja === 'sim' ? 'Igreja: ' + igreja : ''}<br><br>ID de Registro: ${id}`;
+    var data = {
+        nome: nome,
+        dataNascimento: dataNascimento,
+        telefone: telefone,
+        pertenceIgreja: pertenceIgreja,
+        igreja: igreja,
+        id: id
+    };
     
-    document.getElementById('mensagem').innerHTML = mensagem;
-    document.getElementById('resultado').style.display = 'block';
-    document.getElementById('registrationForm').reset();
+    // URL do script do Google Apps
+    var scriptURL = 'https://script.google.com/macros/s/AKfycbygV8HR08cZiVolxt0En6DE7CG9clK_BS7mKgrHRkXfdJ9aRrBa-88bfCkAF2rqmXSW/exec'; // substitua pelo URL gerado
+
+    fetch(scriptURL, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json())
+      .then(data => {
+          if (data.status === 'success') {
+              var mensagem = `Nome: ${nome}<br>Data de Nascimento: ${dataNascimento}<br>Número de Telefone: ${telefone}<br>Pertence a igreja: ${pertenceIgreja}<br>${pertenceIgreja === 'sim' ? 'Igreja: ' + igreja : ''}<br><br>ID de Registro: ${id}`;
+              document.getElementById('mensagem').innerHTML = mensagem;
+              document.getElementById('resultado').style.display = 'block';
+              document.getElementById('registrationForm').reset();
+          } else {
+              alert('Ocorreu um erro ao salvar os dados. Por favor, tente novamente.');
+          }
+      }).catch(error => {
+          console.error('Erro:', error);
+          alert('Ocorreu um erro ao salvar os dados. Por favor, tente novamente.');
+      });
 });
